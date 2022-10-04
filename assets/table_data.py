b1578dca9ac80e46680d2fb365eea15386385b81
Rio <riobeggs1@gmail.com>
2022-10-05 00:16:47 +1300

    working on table setter getter

--- assets/table_data.py
+++ assets/table_data.py
@@ -2,13 +2,35 @@ import assets.web_driver as Driver
 import pandas as pd
 from bs4 import BeautifulSoup
 
-class TableData:
-    def __init__(self, driver=None, table=None): 
-        self.driver = Driver.run_web_driver()
-        self.table = table
 
-    def locate_table(self):
+class TableData:
+    _table = None
+
+    def __init__(self):
+        self.driver = Driver.run_web_driver()
+        self._table = []
+        # self.headings = []
+
+    @property
+    def table(self):
+        """Get our table attribute"""
+        return self._table
+
+    @table.setter
+    def table(self, table: list):
+        """Set our table attribute"""
+        if not isinstance(table, list):
+            raise ValueError("Table has not been located.")
+        self._table = table
+
+    def locate_table(self) -> list:
         main_page = BeautifulSoup(self.driver.page_source, "html.parser")
         find_table = main_page.find("table")
-        self.table = pd.read_html(str(find_table))
+        self.table.append(pd.read_html(str(find_table)))
         self.driver.close()
+        return self.table
+
+    # def column_names(self) -> list:
+    #     for column_name in self.table.find_all("thead"):
+    #         self.headings.append(column_name)
+    #     return self.column_names

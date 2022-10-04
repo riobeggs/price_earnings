93b839823645c8ce945bdc1a3b4aee534391a3ad
Rio <riobeggs1@gmail.com>
2022-10-05 00:15:57 +1300

    refactor

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from assets.table_data import TableData as table_data
+from assets.table_data import TableData
 
 
 def main() -> None:
-    table_data().locate_table()
+    td = TableData()
+    td.table = td.locate_table()
+    print(td.table)
 
+
+
+    # # print(td.table)
+    # td.table = td.locate_table()
+    # print(td.table)
+    # # headings = TableData().column_names()
+    # # print(headings)
 
 if __name__ == "__main__":
     main()
